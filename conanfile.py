715fe0d6ac50701212ecd23228a190e120722d0d
Shaul Fridman <shaul.fridman@gmail.com>
2019-10-01 02:34:49 +0300

    (+) Conan recepie is calling plain vireo build scripts

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
+from conans import ConanFile
 import os
 
 class VireoConan(ConanFile):
@@ -16,21 +15,13 @@ class VireoConan(ConanFile):
         self.run("git clone --depth 1 https://github.com/twitter/vireo")
 
     def build(self):
-        temp_installed_folder = os.path.join(self.build_folder, 'temp_installed')
-        with tools.chdir("./vireo/vireo"):
-            self.run('./configure --prefix={}'.format(temp_installed_folder))        
-            self.run('make')        
+        os.chdir("./vireo/vireo")
+        self.run('./configure --prefix={}'.format(self.package_folder))        
+        self.run('make')        
 
     def package(self):
-        with tools.chdir("./vireo/vireo"):        
-            self.run('make install')       
-        src_include = 'temp_installed/include'
-        src_bin = 'temp_installed/bin'
-        src_lib = 'temp_installed/lib'
-        self.copy(pattern="*", dst="include", src=src_include)
-        self.copy(pattern="*", dst="bin", src=src_bin)
-        self.copy(pattern="*.so*", dst="bin", src=src_lib)
-        self.copy(pattern="*", dst="lib", src=src_lib)
+        os.chdir("./vireo/vireo")
+        self.run('make install')       
 
     def package_info(self):
         self.cpp_info.libs = ["vireo", "imagecore"]
