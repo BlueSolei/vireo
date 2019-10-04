baabcd1c59c0febcd606edbf6646a7e1ca7b72d4
Shaul Fridman <shaul.fridman@gmail.com>
2019-10-04 21:58:50 +0300

    (-) Debug build wsan't complied. adding libjpeg-turbo, fixed it

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
+
 class VireoConan(ConanFile):
     name = "vireo"
     version = "0.1"
@@ -10,19 +11,21 @@ class VireoConan(ConanFile):
     description = "Vireo is a lightweight and versatile video processing library written in C++11"
     topics = ("video processing")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}                                                                                                                 
+    options = {"shared": [True, False]}
     default_options = "shared=False"
+    requires = "libjpeg-turbo/2.0.2@bincrafters/stable"
 
     def source(self):
         repo = tools.Git()
         repo.clone('https://github.com/twitter/vireo', shallow=True)
 
     def build(self):
-        os.chdir("vireo")
-        autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+        os.chdir(f"{self.source_folder}/vireo")
+        autotools = AutoToolsBuildEnvironment(
+            self, win_bash=tools.os_info.is_windows)
         autotools.configure()
         autotools.make()
         autotools.install()
- 
+
     def package_info(self):
         self.cpp_info.libs = ["vireo", "imagecore"]
